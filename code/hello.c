d9e6f7a69e4c1ee5c81f10fe56017bbb039e8c4a
Ali Shehbaz <alishahbaz072@gmail.com>
2025-02-15 15:06:22 -0500

    add notes

--- code/hello.c
+++ code/hello.c
@@ -1,10 +1,8 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main()
-{
-    for (int i=0; i<10; i++){
-	puts("Hello, world!");
-    }
-    return 0;
-
+int main() {
+  for (int i = 0; i < 10; i++) {
+    puts("Hello, world!\n");
+  }
+  return 0;
 }
